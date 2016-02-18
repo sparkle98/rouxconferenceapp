f799b6f7129259dcdcf740b4b27250f7a6d3eb88
Steve P <songprk@gmail.com>
2016-02-18 16:11:18 -0600

    Delete revison at title

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
   </head>
   <body>
-    <h1>Hello, world! rev3</h1>
+    <h1>Hello, world!</h1>
 
     <script src="_/js/bootstrap.js"></script>
     <script src="_/js/myscript.js"></script>
