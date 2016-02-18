91901cea5bf4aaf0c976b6f1cee0cdb53c4a4d55
Steve P <songprk@gmail.com>
2016-02-18 16:33:49 -0600

    Update title as Test Site

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Bootstrap 101 Template</title>
+    <title>GitHub + Heroku Test Site</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
     <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
