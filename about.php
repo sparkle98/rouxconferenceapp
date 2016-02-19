fadb19d89ea449dd90452db632c0372577678b2c
Steve P <songprk@gmail.com>
2016-02-18 22:20:11 -0600

    Complete the basic scaffold for website

--- about.php
+++ about.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>GitHub + Heroku Test Site -- Home</title>
+    <title>GitHub + Heroku Test Site -- About us</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://fonts.googleapis.com/css?family=Bree+Serif|Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
     <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
     <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
   </head>
-  <body id="home">
+  <body id="about">
 
     <section class="container">
       <div class="content row">
